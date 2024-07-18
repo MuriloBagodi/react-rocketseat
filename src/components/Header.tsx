911a281b220cbd6860bd62949380cc483fb92a17
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-07-18 17:17:37 -0300

    Header: stylish off image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,7 @@ import logo from '../assets/Ignite simbol.png'
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <img src={logo} alt="logo ignite" />
-      <strong>Ignite Feed</strong>
+      <img src={logo} alt="Logo do ignite" />
     </header>
   )
 }
