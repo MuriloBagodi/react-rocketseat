fb6b1800aca5ee32478068568013cedb2283e5f7
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-07-21 14:44:28 -0300

    SideBar: Finish all styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,23 @@ import { Header } from './components/Header'
 import { Post } from './components/Post'
 import './global.css'
 
+import styles from './app.module.css'
+import { SideBar } from './components/Sidebar'
+
 function App() {
   return (
     <>
       <Header />
-      <h1>App</h1>
-      <Post
-        title="Teta"
-        author="Murilo Bagodi"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit harum dignissimos dicta neque est dolor at adipisci non a aspernatur reiciendis, reprehenderit sed odio vitae. Odit temporibus quisquam blanditiis perspiciatis!"
-      ></Post>
+      <div className={styles.wrapper}>
+        <SideBar name="Murilo Bagodi" role="Software Engineer" />
+        <main>
+          <Post
+            title="Teta"
+            author="Murilo Bagodi"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit harum dignissimos dicta neque est dolor at adipisci non a aspernatur reiciendis, reprehenderit sed odio vitae. Odit temporibus quisquam blanditiis perspiciatis!"
+          ></Post>
+        </main>
+      </div>
     </>
   )
 }
